322239bf449dc6739560d84dc5b0c59fe00fd0d8
Hotdog <xtomnx@gmail.com>
2020-07-29 21:08:17 +0300

    added highscores and main menu

---
+++ Assets/Scripts/Main_menu/HighScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    [SerializeField]
+    private Text _highScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowHighScore()
+    {
+        _highScoreText.gameObject.SetActive(true);
+        _highScoreText.text = "High Score:" + PlayerPrefs.GetInt("HighScore");
+    }
+}
---
+++ Assets/Scripts/Main_menu/LoadMainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+    [SerializeField]
+    private GameObject _button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_player.gameObject == null)
+        {
+            _button.SetActive(true);
+        }
+    }
+    public void LoadMenu()
+    {
+
+            SceneManager.LoadScene(0);
+        
+    }
+}
